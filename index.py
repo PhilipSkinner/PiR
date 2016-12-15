dcc81eb49161dceca6dda194226551fe046db287
Philip Skinner <me@philip-skinner.co.uk>
2016-12-15 21:20:09 +0000

    adding some config files

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
+
+import lirc
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+  lirc.sendCommand('test', 'test')
+
   return render_template('index.html')
 
 if __name__ == "__main__":
