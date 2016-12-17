4e9f8d3fde28dddd85fec266cdbe86f4e78ad300
Philip Skinner <me@philip-skinner.co.uk>
2016-12-17 16:51:36 +0000

    messing around with hive

--- hive/api.py
+++ hive/api.py
@@ -1,24 +1,131 @@
 import httplib
 import urllib
 import json
+import devices.light
 
-class API():
-    def __init__(self, username, password):
+class API:
+    def __init__(self, username, password):        
         self.username = username
         self.password = password
+        self.token = None
+        self.lights = []
 
         self.login()
-        
-    def getConnection(self):
-        return httplib.HTTPSConnection("beekeeper.hivehome.com")
+        self.getLights()   
 
-    def login(self):
-        print json.dumps({
-            'username' : self.username,
-            'password' : self.password
+    def setBrightness(self, id, value):
+        conn = self.getAPIConnection()
+        conn.request('PUT', '/omnia/nodes/%s' % id, json.dumps({
+            "nodes" : [
+                {
+                    "attributes" : {
+                        "brightness" : {
+                            "targetValue" : value
+                        }
+                    }
+                }
+            ]            
+        }), {
+            'Host' : 'api-prod.bgchprod.info',
+            'Accept' : 'application/vnd.alertme.zoo-6.4+json',
+            'X-AlertMe-Client' : 'Hive Web Dashboard',
+            'Content-Type' : 'application/json',
+            'X-Omnia-Access-Token' : self.token
         })
 
-        conn = self.getConnection()
+        response = conn.getresponse()        
+
+        if response.status != 200:
+            return False            
+        
+        return True 
+
+    def lightOff(self, id):
+        conn = self.getAPIConnection()
+        conn.request('PUT', '/omnia/nodes/%s' % id, json.dumps({
+            "nodes" : [
+                {
+                    "attributes" : {
+                        "state" : {
+                            "targetValue" : "OFF"
+                        }
+                    }
+                }
+            ]            
+        }), {
+            'Host' : 'api-prod.bgchprod.info',
+            'Accept' : 'application/vnd.alertme.zoo-6.4+json',
+            'X-AlertMe-Client' : 'Hive Web Dashboard',
+            'Content-Type' : 'application/json',
+            'X-Omnia-Access-Token' : self.token
+        })
+
+        response = conn.getresponse()        
+
+        if response.status != 200:
+            return False            
+        
+        return True
+    
+    def lightOn(self, id):
+        conn = self.getAPIConnection()
+        conn.request('PUT', '/omnia/nodes/%s' % id, json.dumps({
+            "nodes" : [
+                {
+                    "attributes" : {
+                        "state" : {
+                            "targetValue" : "ON"
+                        }
+                    }
+                }
+            ]            
+        }), {
+            'Host' : 'api-prod.bgchprod.info',
+            'Accept' : 'application/vnd.alertme.zoo-6.4+json',
+            'X-AlertMe-Client' : 'Hive Web Dashboard',
+            'Content-Type' : 'application/json',
+            'X-Omnia-Access-Token' : self.token
+        })
+
+        response = conn.getresponse()
+
+        if response.status != 200:
+            return False
+        
+        return True
+        
+    def getLights(self):
+        #get a list of all of our lights
+        conn = self.getAPIConnection()
+        conn.request('GET', '/omnia/nodes', '', {
+            'Host' : 'api-prod.bgchprod.info',
+            'Accept' : 'application/vnd.alertme.zoo-6.4+json',
+            'X-AlertMe-Client' : 'Hive Web Dashboard',
+            'X-Omnia-Access-Token' : self.token
+        })
+
+        response = conn.getresponse()
+
+        if response.status != 200:
+            return
+
+        data = json.loads(response.read())
+        
+        for node in data['nodes']:            
+            #is it a light?
+            if node['nodeType'] == 'http://alertme.com/schema/json/node.class.light.json#':
+                #yes, init a light object from this node
+                self.lights.append(devices.light.HiveLight(node))
+                
+
+    def getAuthConnection(self):
+        return httplib.HTTPSConnection("beekeeper.hivehome.com")
+
+    def getAPIConnection(self):
+        return httplib.HTTPSConnection("api-prod.bgchprod.info")
+
+    def login(self):
+        conn = self.getAuthConnection()
         conn.request('POST', '/1.0/gateway/login', json.dumps({
             'username' : self.username,
             'password' : self.password
@@ -28,11 +135,14 @@ class API():
             'Content-Type' : 'application/json',
         })
 
-        response = conn.getresponse()
-        print response.status
-        print response.read()
+        response = conn.getresponse()                
         
         if response.status != 200:
             return False
+
+        data = json.loads(response.read())
+
+        #get our access token
+        self.token = data['token']
         
         return True
--- index.py
+++ index.py
@@ -1,13 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 import lirc
 import hive.api
+import sys, getopt
 
+username = None
+password = None
+api = None
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def index():  
+  return render_template('index.html', lights=api.lights)
+
+@app.route('/light/off/<id>')
+def lightOff(id=None):
+  api.lightOff(id)
+  return redirect('/')
+
+@app.route('/light/on/<id>')
+def lightOn(id=None):
+  api.lightOn(id)
+  return redirect('/')
+
+@app.route('/light/brightness/<id>/<level>')
+def lightBrightness(id=None, level=None):
+  api.setBrightness(id, level)
+  return redirect('/')
 
 if __name__ == "__main__":
+  opts, args = getopt.getopt(sys.argv[1:],'u:p:', ['username=', 'password='])  
+
+  for opt, arg in opts:      
+    if opt in ('-u', '--username'):
+      username = arg
+    if opt in ('-p', '--password'):
+      password = arg
+
+  #login to hive
+  api = hive.api.API(username, password)
+
   app.run(host='0.0.0.0', debug=True)
